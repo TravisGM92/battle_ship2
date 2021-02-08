85ed7b81e89adf50b48ce05fe8b2fe5234c3a70c
Travis <mckinstrytg1@gmail.com>
2021-02-08 16:43:20 -0700

    Changes test to expect the content that's been changed

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Home page' do
   it 'Has a home page with styled content' do
     visit('/')
-    expect(page).to have_content('Time to Play Battle Ship')
+    expect(page).to have_content("Time to play\n")
+    expect(page).to have_content("BattleShip!\n")
+    expect(page).to have_button('Play!')
+    expect(page).to have_button('Da Rulez')
+    expect(page).to have_link('About')
   end
 end
