5cd9d795bbd936441dc351b4729e86cc24d65e58
Travis <mckinstrytg1@gmail.com>
2021-02-11 17:42:05 -0700

    Passes spec to allow clicking of rules and play buttons on home page

--- spec/features/game_play_spec.rb
+++ spec/features/game_play_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Play game' do
     it 'when clicked redirects to game play screen' do
       visit('/')
       click_button('Play!')
-      expect(current_path).to eq('/play')
+      expect(current_path).to eq('/game')
     end
   end
 end
