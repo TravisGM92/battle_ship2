52903bc30e8807e2753a3935b5498a549f133e46
Travis <mckinstrytg1@gmail.com>
2021-02-12 13:47:52 -0700

    Passes spec for about page

--- spec/features/about_page_spec.rb
+++ spec/features/about_page_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe 'About page' do
   it 'has content introducing Travis and explaining project' do
     visit('/about')
-    expect(page).to have_content('Travis')
+    expect(page).to have_content("That's me")
+    expect(page).to have_content("This game is a project I challenged myself")
+    expect(page).to have_content("with in order to learn frontend programming")
+    expect(page).to have_css('#gh-page')
   end
 end
