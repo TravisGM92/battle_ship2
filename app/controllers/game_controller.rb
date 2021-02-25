b28764427bae4e95e92655fb0de2a05f8b294fe8
Travis <mckinstrytg1@gmail.com>
2021-02-25 15:14:46 -0700

    Displays users' name on game board after animation

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,6 +4,7 @@ class GameController < ApplicationController
   # creates first user
   def index
     @user = User.find_or_create_by(name: params['user_name'])
+    require "pry"; binding.pry
   end
 
   # rules
