d0db0d123e2a5f29f8a82c74bfa16082c815f03f
Travis <mckinstrytg1@gmail.com>
2021-03-01 16:01:20 -0700

    Creates a setup page with user name in first field

--- spec/features/setup_page_spec.rb
+++ spec/features/setup_page_spec.rb
@@ -12,3 +12,19 @@ RSpec.describe 'Setup game page' do
     end
   end
 end
+
+RSpec.describe 'Setup game page' do
+  describe 'page content' do
+    it 'has a form with users name pre-populated' do
+      visit('/')
+      click_button('Play!')
+      fill_in :user_name, with: 'George'
+      click_button("Let's play")
+      expect(current_path).to eq('/setup')
+      expect(find_field('You').value).to eq('George')
+    end
+    it 'has a form with # of ships, difficulty, and placement of ships' do
+      
+    end
+  end
+end
